abe15f871bff57f7fe46c23b2bc71b3f5a5f2a43
Skylar Robinson <skylardawn150@gmail.com>
2024-10-17 18:56:52 -0700

    Commit 2

--- 210-midterm-starter-1.cpp
+++ 210-midterm-starter-1.cpp
@@ -40,68 +40,68 @@ public:
             return; //end function call
         }
 
-        Node* temp = head;
-        for (int i = 0; i < position && temp; ++i)
-            temp = temp->next;
+        Node* temp = head; //create temp node pointer pointing to head
+        for (int i = 0; i < position && temp; ++i) //loop "position" times
+            temp = temp->next; //set temp pointer to next item in list (temp will be node to add newnode after)
 
-        if (!temp) {
-            cout << "Position exceeds list size. Node not inserted.\n";
-            delete newNode;
-            return;
+        if (!temp) { //if temp is nullptr (position was outside list)
+            cout << "Position exceeds list size. Node not inserted.\n"; //display error message
+            delete newNode; //delete the node we created
+            return; //end function call
         }
 
-        newNode->next = temp->next;
-        newNode->prev = temp;
-        if (temp->next)
-            temp->next->prev = newNode;
-        else
-            tail = newNode;
-        temp->next = newNode;
+        newNode->next = temp->next; //set newnode next pointer to node following temp
+        newNode->prev = temp; //set newnode prev pointer to temp
+        if (temp->next) //if temp is not the end of the list
+            temp->next->prev = newNode; //set the prev pointer of the node after temp to the new node
+        else //temp is the end of the list
+            tail = newNode; //tail points to newnode
+        temp->next = newNode; //the node after temp is now newnode
     }
 
-    void delete_val(int value) {
-        if (!head) return;
+    void delete_val(int value) { //delete a node with a given value
+        if (!head) return; //if list is empty end funciton call
 
-        Node* temp = head;
+        Node* temp = head; //create temporary node pointer to head
         
-        while (temp && temp->data != value)
-            temp = temp->next;
+        while (temp && temp->data != value) //while temp is't nullptr (not the end of the list) and temp data isn't the target value
+            temp = temp->next; //move temp to next list item (temp will be node to delete)
 
-        if (!temp) return; 
+        if (!temp) return; //if value not found end function call
 
-        if (temp->prev)
-            temp->prev->next = temp->next;
-        else
-            head = temp->next; 
+        if (temp->prev) //if temp is not beginning of list
+            temp->prev->next = temp->next; //next ptr of node before temp becomes node after temp
+        else //temp is beginning of list
+            head = temp->next; //head becomes node after temp
 
-        if (temp->next)
-            temp->next->prev = temp->prev;
-        else
-            tail = temp->prev; 
+        if (temp->next) //if temp is not end of list
+            temp->next->prev = temp->prev; //prev of node after temp becomes node before temp
+        else //temp is end of list
+            tail = temp->prev; //tail becomes node before temp
 
-        delete temp;
+        delete temp; //delete temp node
     }
 
-    void delete_pos(int pos) {
-        if (!head) {
-            cout << "List is empty." << endl;
-            return;
+    void delete_pos(int pos) { //delete a node at a given position
+        if (!head) { //if list is empty
+            cout << "List is empty." << endl; //display error message
+            return; //end function call
         }
     
-        if (pos == 1) {
-            pop_front();
-            return;
+        if (pos == 1) { //if position is 1
+            pop_front(); //remove the first list item
+            return; //end function call
         }
     
-        Node* temp = head;
+        Node* temp = head; //create temp node pointer pointing to head
     
-        for (int i = 1; i < pos; i++){
-            if (!temp) {
-                cout << "Position doesn't exist." << endl;
-                return;
+        for (int i = 1; i < pos; i++){ //loop pos - 1 times
+            if (!temp) { //if temp is nullptr (pos out of list)
+                cout << "Position doesn't exist." << endl; //display error message
+                return; //end function call
             }
-            else
-                temp = temp->next;
+            else //if temp isn't out of list
+                temp = temp->next; //temp points to next node in list
         }
         if (!temp) {
             cout << "Position doesn't exist." << endl;
